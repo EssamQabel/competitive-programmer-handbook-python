181a787f4da57df45aadfe3eb7e4d201288a1b66
Essam Qabel <essamqabel@gmail.com>
2020-12-14 18:24:15 +0200

    Updating Chapter 1
    Adding updates and improvements to the chapter 1 python implementation.

--- 01-Introduction/index3.py
+++ 01-Introduction/index3.py
@@ -2,6 +2,7 @@
 
 import math
 
+# INPUT AND OUTPUT
 # Input -> 10 20 monkey
 a, b, c = input().split(' ')
 print(a, b, c)
@@ -14,20 +15,28 @@ print(a, b)
 a, b = map(int, input().split(','))
 print(a, b)
 
+# WORKING WITH NUMBERS
 # Python has dynamic data type so integers, float are same as variables
 
-# Mathematics
+# MATHEMATICS
 # Sum Formulas -> 1 + 2 + 3 ... n = n(n+1)/2
 # Sum Formulas -> 1^k + 2^k + 3^k ... n^k = n(n+1)(2n+1)/6
-# AP -> a + .. + b = n(a+b)/2
-# GP -> a + ak + ak^2 + .. + b = (bk -a) / (k - 1)
+# Arithmetic Progression -> a + .. + b = n(a+b)/2
+# Geometric Progression -> a + ak + ak^2 + .. + b = (bk -a) / (k - 1)
+
 ap = [3, 7, 11, 15]
-print(4*(3+15)/2)
+n = len(ap)
+a = ap[1]
+b = ap[-1]
+print(n*(a+b)/2)
 
-gp = [3, 6, 12, 24] # k is 2 here
-print((24*2 - 3)/(2-1))
+gp = [3, 6, 12, 24]
+a = gp[1]
+b = gp[-1]
+k = 2
+print((b*k - a) / (k - 1))
 
-# Set
+# Set Theory
 x = set([2, 4, 7])
 a = set([1, 2, 5])
 b = set([2, 4])
@@ -35,6 +44,12 @@ print(x)
 print(a.intersection(b))
 print(a.union(b))
 print(a.difference(b))
+
+# Logic 
+# Using the keywords: and, or, not
+print(True and False)
+print(True or False)
+print(not False)
 
 # Functions
 print(3/2)
